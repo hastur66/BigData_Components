9950424690242aa37a9b6959c04804b62a97873d
hastur66 <chamathkaubandara@gmail.com>
2022-12-21 23:35:00 +0530

    adding python connector

---
+++ Cassandra/cassandra_connect.py
@@ -0,0 +1,11 @@
+from cassandra.cluster import Cluster
+
+if __name__=='__main__':
+    cluster = Cluster(['0.0.0.0'], port=9042)
+    session = cluster.connect('cityinfo', wait_for_all_pools=True)
+    session.execute('USE cityinfo')
+    
+    rows = session.execute('SELECT * FROM user')
+    
+    for row in rows:
+        print(row.age, row.name, row.username)
